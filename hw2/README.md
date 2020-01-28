9df7039a1532fbba66fb76583d6ed1ebb624f717
Mike Izbicki <mike@izbicki.me>
2020-01-28 01:04:14 -0800

    hw2

---
+++ hw2/README.md
@@ -0,0 +1,38 @@
+# Intro to pytorch
+
+**Due:** Tuesday, 4 Feb at midnight
+
+## Required tasks
+
+Modify the `classifier.py` file so that:
+
+1. Adjust the training procedure so that the test set is evaluated at the end of every epoch.
+
+1. Implement two new models: the random feature model and the 1 hidden layer neural network model.
+   You should add a command line argument `--model` which takes one of three options
+   (`linear`, `random_feature`, and `nn`)
+   and another argument `--size` which takes takes an integer argument and controls the number of random features or the size of the hidden layer, depending on the model.
+   This will require modifying the `define the model` and the `optimization` sections of the homework file.
+
+You should experiment with different values of `--alpha`, `--epochs`, `--batch_size`, `--model`, and `--size` to see how they effect your training time and the resulting accuracy of your models.
+Try to find the combination that results in the best training accuracy.
+
+## Recommended tasks
+
+You are not required to complete the following tasks,
+however they are good exercises to get you familiar with pytorch.
+
+1. Currently, the print statement of the inner loop of the optimization prints the loss of a single batch of data.
+   Because this is only a single batch of data, the loss value is highly noisy, and it is difficult to tell if the model is converging.
+   The [exponential moving average](https://en.wikipedia.org/wiki/Moving_average) is a good way to smooth these values, 
+   and machine learning practitioners typically use this technique to smooth the training loss and measure convergence.
+   Implement this technique in your `classifier.py` file.
+
+1. Add a command line option to use the MNIST dataset instead of CIFAR10 for training and testing.
+   (This will require changing code in both the `load dataset` and the `define the model` sections of code.)
+   Torchvision has many other datasets as well (see https://pytorch.org/docs/stable/torchvision/datasets.html), and you can add these datasets too. 
+
+## Submission
+
+Upload your `classifier.py` file to sakai
+
---
+++ hw2/classifier.py
@@ -0,0 +1,101 @@
+#!/bin/python3
+
+# process command line args
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size',type=int,default=16)
+parser.add_argument('--alpha',type=float,default=0.01)
+parser.add_argument('--epochs',type=int,default=10)
+parser.add_argument('--show_image',action='store_true')
+args = parser.parse_args()
+
+# imports
+import datetime
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+# load dataset
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+trainset = torchvision.datasets.CIFAR10(
+    root = './data',
+    train = True,
+    download = True,
+    transform = transform,
+    )
+trainloader = torch.utils.data.DataLoader(
+    trainset,
+    batch_size = args.batch_size,
+    shuffle = True,
+    )
+
+testset = torchvision.datasets.CIFAR10(
+    root = './data',
+    train = True,
+    download = True,
+    transform = transform,
+    )
+testloader = torch.utils.data.DataLoader(
+    testset,
+    batch_size = args.batch_size,
+    shuffle = True,
+    )
+
+# display images
+if args.show_image:
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    def imshow(img):
+        img = img / 2 + 0.5     # unnormalize
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
+
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+
+# define the model
+w = torch.tensor(torch.rand(3,32,32,10),requires_grad=True)
+b = torch.tensor(torch.rand(10),requires_grad=True)
+
+def f(x):
+    return torch.einsum('bijk,ijkl -> bl',x,w)+b
+
+# optimize 
+
+criterion = nn.CrossEntropyLoss()
+
+loss=float('inf')
+for epoch in range(args.epochs):
+    for i, data in enumerate(trainloader, 0):
+        if i%1000==0:
+            print(datetime.datetime.now(),'epoch=',epoch,'i=',i,'loss=',loss)
+        images, labels = data
+        outputs = f(images)
+        loss = criterion(outputs,labels)
+        loss.backward()
+        w = w - args.alpha * w.grad
+        b = b - args.alpha * b.grad
+        w = torch.tensor(w,requires_grad=True)
+        b = torch.tensor(b,requires_grad=True)
+
+# eval on test set
+correct = 0.0
+total = 0.0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = f(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('test set accuracy = ', correct/total)
